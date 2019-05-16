67daba142ecbe2d165f9aa28111c770026b2c2d4
Ctrl_Null <raphael@fonseca.tech>
2019-05-15 21:03:18 -0400

    created constants and setup to check if json is setup corrently

--- termInit.js
+++ termInit.js
@@ -1,4 +1,23 @@
-const botconfig = require("./botconfig.json");
+const BOTCONF = require("./botconfig.json");
+const CONSTS = require("./consts.json");
 const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 
-bot.loging(botconfig.token)
+bot.login(BOTCONF.token).then(() => {
+    console.log("I am ready");
+    // Get Guild ID's
+    var ROBPlayground = client.guilds.get(CONSTS.robPlayground['guildId']);
+    console.log(ROBPlayground);
+
+    var TEKy = client.guild.get(CONSTS.tekies['guildId']);
+    console.log(TEKy)
+
+    // if (guild && guild.channels.get(channelId)) {
+    //     guild.channels.get(channelId).send("Terminator Initialized").then(() => client.destroy());
+    // } else {
+    //     console.log("nope");
+    //     //if the bot doesn't have guild with the id guildid
+    //     // or if the guild doesn't have the channel with id channelid
+    // }
+    client.destroy();
+});
