406444349ec289f3d567064f4a0d7c556474c5d9
Ctrl_Null <raphael@fonseca.tech>
2019-05-15 23:17:43 -0400

    forgot to enable command

--- termPurge.js
+++ termPurge.js
@@ -6,15 +6,15 @@ const bot = new Discord.Client();
 
 bot.login(BOTCONF.token).then(() => {
     console.log("I am ready");
-    // // Get Guild ID's
-    // var ROBPlayground = bot.guilds.get(CONSTS.robPlayground['guildId']);
+    // Get Guild ID's
+    var ROBPlayground = bot.guilds.get(CONSTS.robPlayground['guildId']);
     
-    // if (ROBPlayground && ROBPlayground.channels.get(CONSTS.robPlayground['channels'][0]['muzak'])) {
-    //     ROBPlayground.channels.get(CONSTS.robPlayground['channels'][0]['muzak']).send("*OBLITERATE").then(() => console.log("RobPlayground Init"));
-    //     ROBPlayground.channels.get(CONSTS.robPlayground['channels'][0]['muzak']).send("All items Terminated").then(() => console.log("RobPlayground Init"));
-    // }else {
-    //     console.log("ROBs Failed")
-    // }
+    if (ROBPlayground && ROBPlayground.channels.get(CONSTS.robPlayground['channels'][0]['muzak'])) {
+        ROBPlayground.channels.get(CONSTS.robPlayground['channels'][0]['muzak']).send("*terminate").then(() => console.log("RobPlayground Init"));
+        ROBPlayground.channels.get(CONSTS.robPlayground['channels'][0]['muzak']).send("All items Terminated").then(() => console.log("RobPlayground Init"));
+    }else {
+        console.log("ROBs Failed")
+    }
     
     var TEKy = bot.guilds.get(CONSTS.tekies['guildId']);
     if (TEKy && TEKy.channels.get(CONSTS.tekies['channels'][0]['pr001'])) {
