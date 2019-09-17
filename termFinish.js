0e6005f0a68b16da63cbc039876d709a1c4f81a5
Raphael Fonseca <rfonseca@hileman.biz>
2019-09-17 10:07:24 -0400

    [term10 & termFinish.js] added the carloinas statement

--- termFinish.js
+++ termFinish.js
@@ -7,6 +7,8 @@ const bot = new Discord.Client();
 bot.login(BOTCONF.token).then(() => {
     console.log("I am ready");
     // Get Guild ID's
+
+    // Hangout Server
     var ROBPlayground = bot.guilds.get(CONSTS.robPlayground['guildId']);
     
     if (ROBPlayground && ROBPlayground.channels.get(CONSTS.robPlayground['channels'][0]['muzak'])) {
@@ -15,11 +17,22 @@ bot.login(BOTCONF.token).then(() => {
         console.log("ROBs Failed")
     }
     
+    // Tekies Server
     var TEKy = bot.guilds.get(CONSTS.tekies['guildId']);
     if (TEKy && TEKy.channels.get(CONSTS.tekies['channels'][0]['pr001'])) {
         TEKy.channels.get(CONSTS.tekies['channels'][0]['pr001']).send("Purge Complete").then(() => console.log('Tekies initSent'));
     } else {
         console.log("Failed");
     }
+
+    // The Carolina's Server 
+    var carolinaServ = bot.guilds.get(CONSTS.theCarolinas['guildId']);
+    if (carolinaServ && carolinaServ.channels.get(CONST.theCarolinas['channels'][0]['private001'])) {
+        carolinaServ.channels.get(CONSTS.tekies['channels'][0]['pr001']).send("Purge Complete").then(() => console.log('The Carolinas warnning sent'));
+    } else {
+        console.log("Failed");
+    }
+
+
     bot.destroy();
 });
