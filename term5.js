87619a6df5d89cbf1ddb91278ea0b6b893e35f5a
Raphael Fonseca <rfonseca@hileman.biz>
2019-09-17 10:03:50 -0400

    updating stuff
    [consts.json] udpated json to accomidate naming
    [term5.js] added statemant for carolina servers

--- term5.js
+++ term5.js
@@ -7,6 +7,8 @@ const bot = new Discord.Client();
 bot.login(BOTCONF.token).then(() => {
     console.log("I am ready");
     // Get Guild ID's
+
+    // Hangout house
     var ROBPlayground = bot.guilds.get(CONSTS.robPlayground['guildId']);
     
     if (ROBPlayground && ROBPlayground.channels.get(CONSTS.robPlayground['channels'][0]['muzak'])) {
@@ -14,12 +16,21 @@ bot.login(BOTCONF.token).then(() => {
     }else {
         console.log("ROBs Failed")
     }
-    
+    // Tekies Server
     var TEKy = bot.guilds.get(CONSTS.tekies['guildId']);
     if (TEKy && TEKy.channels.get(CONSTS.tekies['channels'][0]['pr001'])) {
         TEKy.channels.get(CONSTS.tekies['channels'][0]['pr001']).send("5 minutes until TERMINATION").then(() => console.log('Tekies initSent'));
     } else {
         console.log("Failed");
     }
+
+    // The Carolina's Server 
+    var carolinaServ = bot.guilds.get(CONSTS.theCarolinas['guildId']);
+    if (carolinaServ && carolinaServ.channels.get(CONST.theCarolinas['channels'][0]['private001'])) {
+        carolinaServ.channels.get(CONSTS.tekies['channels'][0]['pr001']).send("5 minutes until TERMINATION").then(() => console.log('The Carolinas warnning sent'));        
+    } else {
+        console.log("Failed");
+    }
+
     bot.destroy();
 });
