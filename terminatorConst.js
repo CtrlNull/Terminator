92bb10850fbe684ae45abde70298b7fe10f738fa
ctrl_null <raphael@fonseca.tech>
2019-09-23 08:48:10 -0400

    [terminatorConst.j] updated roles checker and message

--- terminatorConst.js
+++ terminatorConst.js
@@ -74,8 +74,8 @@ bot.on('message', message => {
             message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
 
             if (message.channelid == CONSTS.theCarolinas['channels'][0]['private001']) {
-                if ((!message.member.roles.find("name", "Terminator")) || (!message.member.roles.find("name", "AdminZ"))) {
-                    message.channel.send('You need the \`Terminator\` role to use this command.');
+                if ((!message.member.roles.find("name", "Terminator"))|| (!message.member.roles.find("name", "BOT")) || (!message.member.roles.find("name", "AdminZ"))) {
+                    message.channel.send('You need the \`Terminator\` or \`AdminZ\` role to use this command.');
                 }
 
                 if (!message.member.roles.find("name", "AdminZ")) {}
